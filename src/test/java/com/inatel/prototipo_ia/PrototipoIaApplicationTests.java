3ca7976f3861ad2105ef86866c038c3cc9fd19da
Davi Rosa Gomes <davi.rosa@gec.inatel.br>
2025-10-30 08:20:20 -0300

    Democracia Corinthiana

--- src/test/java/com/inatel/prototipo_ia/PrototipoIaApplicationTests.java
+++ src/test/java/com/inatel/prototipo_ia/PrototipoIaApplicationTests.java
@@ -1,12 +1,7 @@
 package com.inatel.prototipo_ia;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = com.inatel.prototipo_ia.PrototipoIaApplication.class,
-    properties = {
-        "spring.jpa.hibernate.ddl-auto=none"
-    })
 public class PrototipoIaApplicationTests {
 
 	@Test
